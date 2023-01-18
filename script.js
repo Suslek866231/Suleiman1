ffe89db54a69b57a2125265a5a241af58716194e
Suslek866231 <122853302+Suslek866231@users.noreply.github.com>
2023-01-18 18:46:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//--------------------------------1
+//Перменные: years,myDog,quests,
+//Функции; makeTea,secret,dogYears
+//Встроенные функции;substr,substring,slice,console.log,
+//Аргументы;(myDog, 4),(guests, 'Earl Grey'),
+//Параметры;(dogname, age),(cups, tea),
+//--------------------------------2
+//let txt = 'JS';
+//console.log(txt.toLowerCase());
+//--------------------------------3
+// let Chevapchich = "я люблю JS!";
+// console.log(Chevapchich.substr(0,1));
+// console.log(Chevapchich.substring(0,1));
+// console.log(Chevapchich.slice(0,1));
+//--------------------------------4
+//let Petr = "я люблю JS!"; {
+//    console.log (Petr.indexOf("люблю"))
+//}
+//--------------------------------5
+//let Gorshok = "любой текст";
+//let n = 6;
+//let result = 0
+//if(n<Gorshok.length){
+//    result = Gorshok.substr(0,11);
+//    console.log(result + "...");
+//}else {
+//    result=Gorshok;
+//    console.log(result);
+//}
+//--------------------------------6
+//let txt = "Я-люблю-JS!";
+//console.log(txt.replace(/-/g, '!'));
+//--------------------------------7
+//let mandarin = "я люблю JS"
+//console.log(mandarin.split(''));
+//--------------------------------8
+//let rock = "Привет мир";
+//console.log(rock.split(""))
+
